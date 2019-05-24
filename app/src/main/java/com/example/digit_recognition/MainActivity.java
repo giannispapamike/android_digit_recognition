d5e751cb3e964f8a84ed3824e796d7c2ac29ce4e
mstath <mstath@cslab.ece.ntua.gr>
2019-05-24 18:13:37 +0300

    [add]

--- app/src/main/java/com/example/digit_recognition/MainActivity.java
+++ app/src/main/java/com/example/digit_recognition/MainActivity.java
@@ -14,78 +14,128 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 
 
+
+import android.util.DisplayMetrics;
+import android.view.MenuInflater;
+
+import java.util.function.ToLongBiFunction;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
-    private ImageView imageView;
+//    private static final int REQUEST_IMAGE_CAPTURE = 1;
+//    private ImageView imageView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        /* Photo */
+//        imageView = findViewById(R.id.photo);
+//
+//        setSupportActionBar(toolbar);
+//
+////        FloatingActionButton fab = findViewById(R.id.fab);
+////        fab.setOnClickListener(new View.OnClickListener() { onClick
+////        });
+//    }
+//
+//
+//    public void takePicture(View view) {
+//        /*
+//            Intent: abstract description of an operation to be performed
+//            communicate with different applications
+//        */
+//        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        /* Check if there is ANY activity that can resolve this intent */
+//        if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
+//            /* Start Activity */
+//            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//
+//        /* Camera Case && Operation was successful */
+//        if ((requestCode == REQUEST_IMAGE_CAPTURE) && (resultCode == Activity.RESULT_OK)) {
+//            /* Android Camera application encodes the photo as a small Bitmap
+//               in the extras, under the key "data" */
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            /* Assign bitmap to view aka Display the photo */
+//            imageView.setImageBitmap(imageBitmap);
+//        }
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    public void hello(View v) {
+//        Log.d("TEST", "Hello World!");
+//    }
+
+    private PaintView paintView;
+    private Toolbar mTopToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        /* Photo */
-        imageView = findViewById(R.id.photo);
+        mTopToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mTopToolbar);
 
-        setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() { onClick
-//        });
-    }
-
-
-    public void takePicture(View view) {
-        /*
-            Intent: abstract description of an operation to be performed
-            communicate with different applications
-        */
-        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        /* Check if there is ANY activity that can resolve this intent */
-        if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
-            /* Start Activity */
-            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        /* Camera Case && Operation was successful */
-        if ((requestCode == REQUEST_IMAGE_CAPTURE) && (resultCode == Activity.RESULT_OK)) {
-            /* Android Camera application encodes the photo as a small Bitmap
-               in the extras, under the key "data" */
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            /* Assign bitmap to view aka Display the photo */
-            imageView.setImageBitmap(imageBitmap);
-        }
+        paintView = (PaintView) findViewById(R.id.paintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.normal:
+                paintView.normal();
+                return true;
+            case R.id.emboss:
+                paintView.emboss();
+                return true;
+            case R.id.blur:
+                paintView.blur();
+                return true;
+            case R.id.clear:
+                paintView.clear();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void hello(View v) {
-        Log.d("TEST", "Hello World!");
     }
 }
